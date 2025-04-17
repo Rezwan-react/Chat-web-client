c6fa6855102687e3e6ff217c731b31c6c1817224
Rezwan-react <mdrazwanislam8@gmail.com>
2025-04-17 16:33:36 +0600

    Chat com

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import { FaRegPaperPlane } from 'react-icons/fa'
 
 function Chat() {
   return (
-    <h1>Chat</h1>
+    <>
+      <section className='w-full h-screen'>
+        <div className="container">
+          <div className='chat_text  '>
+            <div className="userDataBar p-5 bg-[#FDFAF6]">
+              <div className="singel_users flex justify-between mb-5 items-center ">
+                <div className='flex items-center gap-5'>
+                  <div className=" bg-green-100 user_image w-[50px] h-[50px] rounded-full overflow-hidden">
+                    <img src="/img/rezwan.jpg" alt="user photo" />
+                  </div>
+                  <h2 className='text-lg font-semibold text-[#222222]'>friendName</h2>
+                </div>
+              </div>
+            </div>
+            <div className="msg_box w-full h-[500px]  p-5 overflow-y-scroll">
+              <div className='mb-5 '>
+                <p className="sender_msg w-fit py-1 px-3  ml-auto bg-[#6A9AB0] rounded-xl font-Poppins text-white">message</p>
+                <p className="sender_msg w-fit py-1 px-3  ml-auto  rounded-xl text-sm  font-Poppins text-black">messageTime</p>
+              </div>
+              <div className='mb-5 '>
+                <p className="recive_msg w-fit py-1 px-3 bg-[#3A6D8C] rounded-xl font-Poppins text-white">message</p>
+                <p className="sender_msg w-fit py-1 px-3    rounded-xl text-sm font-Poppins text-black">messageTime</p>
+              </div>
+            </div>
+            <div className="msg_input flex px-10  border-t-2 border-t-[#640D5F]  ">
+              <input
+                type="text" className='w-full bg-transparent text-xl font-bold font-Poppins text-[#522258] mt-4 outline-none ' placeholder='Enter your message' />
+              <button
+                className='text-xl text-[#2E236C] active:scale-[1.1] '><FaRegPaperPlane /></button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,18 +2,27 @@ import React from 'react'
 import { CgMenuRound } from 'react-icons/cg'
 import { CiChat1 } from 'react-icons/ci'
 import { GrGroup } from 'react-icons/gr'
-import { Link, NavLink } from 'react-router'
+import { Link, Links, NavLink } from 'react-router'
 
 function Navbar() {
     return (
         <nav>
             <div className="container">
-                <div className="w-[192px] h-screen bg-[#3D90D7] px-6 pt-24">
+                <div className="w-[192px] h-screen bg-[#3D90D7] px-6 pt-24 flex flex-col justify-between">
                     <ul className="flex flex-col gap-6 text-2xl font-medium font-poppins text-[#222222] ">
                         <NavLink to="/chat" className={({ isActive }) => isActive ? "flex items-center  gap-2 px-[19px] py-[16px] bg-[#32375C] text-[#fff] rounded-xl " : "flex items-center gap-2 px-[19px] py-[16px] hover:text-gray-200 transition-colors duration-200"}><CiChat1 className='text-2xl' /><span>Chat</span> </NavLink>
                         <NavLink to="/group" className={({ isActive }) => isActive ? "flex items-center  gap-2 px-[19px] py-[16px] bg-[#32375C] text-[#fff] rounded-xl " : "flex items-center gap-2 px-[19px] py-[16px] hover:text-gray-200 transition-colors duration-200"}><GrGroup className='text-2xl' /><span>Group</span> </NavLink>
                         <NavLink to="/people" className={({ isActive }) => isActive ? "flex items-center  gap-2 px-[19px] py-[16px] bg-[#32375C] text-[#fff] rounded-xl " : "flex items-center gap-2 px-[19px] py-[16px] hover:text-gray-200 transition-colors duration-200"}><CgMenuRound className='text-2xl' /><span>People</span> </NavLink>
                     </ul>
+                    <div className='flex items-center gap-2'>
+                        <div className='w-12 h-12 rounded-full broder-[1px] border-rose-500 overflow-hidden'>
+                            <img src="/img/rezwan.jpg" alt="img" />
+                        </div>
+                        <div>
+                            <h2 className='text-[18px] font-semibold font-poppins text-[#000]'>Rezwan</h2>
+                            <Link className='text-[13px] font-normal font-poppins text-[#000]' to="#">Edit Profile</Link>
+                        </div>
+                    </div>
                 </div>
             </div>
         </nav>
