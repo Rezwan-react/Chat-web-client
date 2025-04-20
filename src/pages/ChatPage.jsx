9ea554eb8ea18dfee4a0e6a413f430ee193ccc7d
Rezwan-react <mdrazwanislam8@gmail.com>
2025-04-20 21:47:40 +0600

    chat and people flex

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import Chat from '../components/chat/Chat'
+import People from '../components/people/People'
 
 function ChatPage() {
-    return (
-        <>
-            <Chat />
-        </>
-    )
+  return (
+    <div className='flex w-full min-h-screen'>
+      {/* Sidebar */}
+      <div className="w-[320px] bg-[#e8e8e8] border-r">
+        <People />
+      </div>
+
+      {/* Main Chat Area */}
+      <div className="flex-1 bg-white">
+        <Chat />
+      </div>
+    </div>
+  )
 }
 
 export default ChatPage
