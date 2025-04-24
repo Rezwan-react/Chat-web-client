e605252ecbaf420e54c94df1d34f57c6072e562e
Rezwan-react <mdrazwanislam8@gmail.com>
2025-04-25 00:39:37 +0600

    register connection saver

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,30 @@
-import { BrowserRouter, Route, Routes } from 'react-router'
-import './App.css'
-import Layout from './components/layout/Layout'
-import ChatPage from './pages/ChatPage'
-import GroupPage from './pages/GroupPage'
-import PeoplePage from './pages/PeoplePage'
-import OtpVerifyPage from './pages/OtpVerifyPage'
-import LoginPage from './pages/LoginPage'
-import RegisterPage from './pages/RegisterPage'
-
+import { BrowserRouter, Route, Routes } from "react-router";
+import "./App.css";
+import Layout from "./components/layout/Layout";
+import ChatPage from "./pages/ChatPage";
+import GroupPage from "./pages/GroupPage";
+import PeoplePage from "./pages/PeoplePage";
+import OtpVerifyPage from "./pages/OtpVerifyPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/register' element={<RegisterPage/>} />
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/' element={<Layout />}>
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<Layout />}>
             <Route index element={<ChatPage />} />
-            <Route path='/group' element={<GroupPage />} />
-            <Route path='/people' element={<PeoplePage />} />
-            <Route path='/otpVerifyPage' element={<OtpVerifyPage />} />
+            <Route path="/group" element={<GroupPage />} />
+            <Route path="/people" element={<PeoplePage />} />
+            <Route path="/otpVerifyPage" element={<OtpVerifyPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
